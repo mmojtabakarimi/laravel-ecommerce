db47a14571f9bff37f0b51066b527a5f05aa3a09
root <mk_prg@yahoo.com>
2021-03-07 11:30:37 +0330

    Add resource foledr

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,6 @@
+<footer>
+    <div class="footer-content container">
+        <div class="made-with">Created By Mojtaba Karimi</div>
+        {{ menu('footer', 'partials.menus.footer') }}
+    </div> <!-- end footer-content -->
+</footer>
